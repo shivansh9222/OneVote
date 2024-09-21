1843e957f352211bdc8c05379fb800ad0efadfc3
Anmol Rai <purevoidexists@gmail.com>
2024-09-21 20:11:24 +0530

    header initialsed

---
+++ OneVote/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Link , NavLink} from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <header className="sticky shadow p-2 bg-orange-400">
+            
+        </header>
+    );
+}
