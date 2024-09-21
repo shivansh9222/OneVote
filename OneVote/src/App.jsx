806b93ac0608ef631a654076ba56435cd180d0d8
Anmol Rai <purevoidexists@gmail.com>
2024-09-21 20:00:07 +0530

    Intitalized one-vote project

---
+++ OneVote/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-blue-400 text-black font-semibold p-2 text-xl text-center'>One Vote</h1>
+    </>
+  )
+}
+
+export default App
