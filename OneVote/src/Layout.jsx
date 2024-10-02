87a880032be100feef9b362110a3ae3574a3eb9b
Anmol Rai <purevoidexists@gmail.com>
2024-10-02 12:40:16 +0530

    Created a Layout of the view

---
+++ OneVote/src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Outlet } from "react-router-dom";
+import { Header,Footer } from "./components";
+
+function Layout() {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer/>
+        </>
+    )
+
+}
+
+export default Layout;
