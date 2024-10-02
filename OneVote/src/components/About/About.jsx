207c5690cbd6bd76dd93e59be24b984c712f56b3
Anmol Rai <purevoidexists@gmail.com>
2024-10-02 12:41:29 +0530

    Dummy update

---
+++ OneVote/src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return(
+        <>
+            <div className="bg-gray-500 h-[40vh] w-full text-center text-2xl">About us page is this</div>
+        </>
+    )
+}
+
+export default About;
---
+++ OneVote/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return(
+        <>
+            <div className='h-[70vh] w-full bg-gray-500 text-2xl text-center'>
+                This is Home section
+            </div>
+        </>
+    );
+}
+
+export default Home;
