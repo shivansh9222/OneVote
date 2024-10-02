41dd46363d6b1abc95fcfc55b75d13445228358b
Anmol Rai <purevoidexists@gmail.com>
2024-10-02 12:40:28 +0530

    export files updated

--- OneVote/src/components/index.js
+++ OneVote/src/components/index.js
@@ -1,3 +1,7 @@
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Home from "./Home/Home";
+import About from "./About/About";
+import User from "./User/User"
 
-export {Header};
+export {Header,Footer , Home,About , User};
