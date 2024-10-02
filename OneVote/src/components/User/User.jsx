fc9d004d1d5961a1cbbbf92673c6d7674961de72
Anmol Rai <purevoidexists@gmail.com>
2024-10-02 12:41:13 +0530

    Dummy for future User section

---
+++ OneVote/src/components/User/User.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function User(){
+    const {userId} = useParams();
+    return(
+        <>
+            <div className="w-full h-1/2 text-center bg-gray-500 text-2xl">User: {userId}</div>
+        </>
+    )
+}
+
+export default User;
