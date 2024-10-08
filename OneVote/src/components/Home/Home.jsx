e17a0efca0a487bfcfb43a0c8e04b1873bda572d
Anmol Rai <purevoidexists@gmail.com>
2024-10-08 20:03:32 +0530

    added functionality for onVote and addition of the total Vote counts into Party data in partiesData file

--- OneVote/src/components/Home/Home.jsx
+++ OneVote/src/components/Home/Home.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-
+import Card from './Card/Card';
+import Parties from '../../assests/partyData';
 function Home() {
+
+    const handleVote = (cardId) => {
+        for(let i = 0; i<Parties.length; i++){
+            if(Parties[i].id === cardId){
+                Parties[i].totalVote += 1;
+                break;
+            }
+        }
+    }
     return(
-        <>
-            <div className='h-[70vh] w-full bg-gray-500 text-2xl text-center'>
-                This is Home section
-            </div>
-        </>
+        <main 
+            className='w-full grid grid-cols-1 gap-5 justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'
+        >
+            {
+                Parties.map((party,index)=>{
+                    return(
+                        <div key={index}>
+                            <Card 
+                                name={party.name}
+                                logo={party.logo}
+                                description={party.description}
+                                manifestoLink={party.manifestoLink}
+                                onVote={() => {
+                                    handleVote(party.id);
+                                    console.log(party.name , ":",party.totalVote);
+                                }}
+                            />
+                        </div>
+                    )
+                })
+            }
+        </main>
     );
 }
 
