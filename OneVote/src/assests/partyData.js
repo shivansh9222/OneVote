0e837f9fc1b02d244e42194cec050b757edba5d9
Anmol Rai <purevoidexists@gmail.com>
2024-10-08 20:03:59 +0530

    adding parties data

---
+++ OneVote/src/assests/partyData.js
@@ -0,0 +1,22 @@
+const Parties = [
+    {
+        id:1,
+        totalVote: 0,
+        name: "BJP",
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Bharatiya_Janata_Party_logo.svg',
+        description: "Bharatiya Janata Party is an organisation built on certain principles. Its strength is its cadre. It is not centered around any individual, leader, family or dynasty. Its driving force is national approach to all issues rising above caste and religion.",
+        manifestoLink: 'https://www.bjp.org/'
+        
+    },
+    {
+        id:2,
+        totalVote: 0,
+        name: 'CONGRESS',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Indian_National_Congress_hand_logo.png',
+        description: "Founded on 28 December 1885, it was the first modern nationalist movement to emerge in the British Empire in Asia and Africa. From the late 19th century, and especially after 1920, under the leadership of Mahatma Gandhi",
+        manifestoLink: 'https://manifesto.inc.in/'
+        
+    }
+];
+
+export default Parties;
