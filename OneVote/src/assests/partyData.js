6089359140843d866cf6f01b05f5a62a6f35de2e
Anmol Rai <purevoidexists@gmail.com>
2024-10-08 20:13:26 +0530

    added few party data

--- OneVote/src/assests/partyData.js
+++ OneVote/src/assests/partyData.js
@@ -6,7 +6,6 @@ const Parties = [
         logo: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Bharatiya_Janata_Party_logo.svg',
         description: "Bharatiya Janata Party is an organisation built on certain principles. Its strength is its cadre. It is not centered around any individual, leader, family or dynasty. Its driving force is national approach to all issues rising above caste and religion.",
         manifestoLink: 'https://www.bjp.org/'
-        
     },
     {
         id:2,
@@ -15,8 +14,31 @@ const Parties = [
         logo: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Indian_National_Congress_hand_logo.png',
         description: "Founded on 28 December 1885, it was the first modern nationalist movement to emerge in the British Empire in Asia and Africa. From the late 19th century, and especially after 1920, under the leadership of Mahatma Gandhi",
         manifestoLink: 'https://manifesto.inc.in/'
-        
-    }
+    },
+    {
+        id:3,
+        totalVote: 0,
+        name: 'APP',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Aam_Aadmi_Party_logo_%28English%29.svg/1280px-Aam_Aadmi_Party_logo_%28English%29.svg.png',
+        description: "The Aam Aadmi Party (transl. Common Man's Party; abbr. AAP) is a political party in India. It was founded on 26 November 2012 by Arvind Kejriwal and his then-companions,[23] following the 2011 Indian anti-corruption movement against then Indian government of Indian National Congress.",
+        manifestoLink: 'https://aamaadmiparty.org/'
+    },
+    {
+        id:4,
+        totalVote: 0,
+        name: 'BSP',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Elephant_Bahujan_Samaj_Party.svg/1024px-Elephant_Bahujan_Samaj_Party.svg.png',
+        description: "The Bahujan Samaj Party (abbr. BSP) is a political party in India that was formed to represent Bahujans (literally means \"community in majority\"), referring to Scheduled Castes, Scheduled Tribes, and Other Backward Classes (OBC), along with religious minorities",
+        manifestoLink: 'https://manifesto.inc.in/'
+    },
+    {
+        id:5,
+        totalVote: 0,
+        name: 'CPI(M)',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Communist_Party_of_India_Symbol.png',
+        description: "The Communist Party of India (CPI) is the oldest communist party in India. The CPI was founded in modern-day Kanpur on 26 December 1925",
+        manifestoLink: 'https://cpim.org/'
+    },
 ];
 
 export default Parties;
