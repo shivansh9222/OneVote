217d10486aca242a5ae06b5dbb71d59d9fba9070
Anmol Rai <purevoidexists@gmail.com>
2024-10-08 20:02:46 +0530

    added more Card displays

--- OneVote/src/components/Home/Card/Card.jsx
+++ OneVote/src/components/Home/Card/Card.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import './Card.css'
 
-function Card({name , logo, description}){
-    const [vote , setVote] = useState(1);
+function Card({name , logo, description, manifestoLink , onVote}){
     return (
-        <main className="cardContainer" >
+        <main 
+            className="cardContainer flex mb-4"
+            
+        >
             <div className="card">
                 <div className="theFront p-2">
                     <div className="w-full h-full flex items-center justify-center p-2 box-border border-[3px] border-orange-500 rounded-lg ">
@@ -15,16 +17,26 @@ function Card({name , logo, description}){
                         />
                     </div>
                 </div>
-                <div className="theBack flex flex-col items-center justify-around p-2 gap-y-10 bg-orange-400 text-white">
-                    <h1 className="text-2xl ubuntu-medium-italic">{name}</h1>
-                    <p className="text-center text-white ubuntu-medium-italic">{description}</p>
+                <div 
+                    className="theBack flex flex-col items-center justify-around p-2 gap-y-10 bg-orange-400 text-white"
+                >
+                    <h1 className="text-2xl ubuntu-medium-italic">
+                        {name}
+                    </h1>
+                    <p 
+                        className="text-center text-white ubuntu-medium-italic"
+                    >
+                        {description}
+                    </p>
+                    <Link 
+                        to={manifestoLink}
+                        className="text-blue-500 underline ubuntu-light-italic hover:text-blue-700"
+                    >
+                        Menifesto
+                    </Link>
                     <button 
                         className="text-orange-500 bg-white hover:bg-gray-600 hover:text-white ubuntu-medium-italic p-2 rounded-lg shadow-md transition-all ease-in-out duration-200" 
-                        onClick={() => {
-                                setVote(vote + 1);
-                                console.log(vote);
-                            }
-                        }
+                        onClick={onVote}
                     >
                         Vote
                     </button>
