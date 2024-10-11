e09a693f21b8d4d8ee7f2f1ffd34d4516081413f
Anmol Rai <purevoidexists@gmail.com>
2024-10-11 20:17:51 +0530

    minor practice changes

--- OneVote/src/components/User/User.jsx
+++ OneVote/src/components/User/User.jsx
@@ -6,6 +6,9 @@ function User(){
     return(
         <>
             <div className="w-full h-1/2 text-center bg-gray-500 text-2xl">User: {userId}</div>
+            {/* <Login /> */}
+            {/* <SignUp /> */}
+            {/* <Registeration /> */}
         </>
     )
 }
