bf24c40635319016cef8e3f24c886bd4db1b769d
Anmol Rai <purevoidexists@gmail.com>
2024-10-11 20:15:34 +0530

    Routing style changes

--- OneVote/src/App.jsx
+++ OneVote/src/App.jsx
@@ -1,18 +1,62 @@
 import { createBrowserRouter, createRoutesFromElements, Route , RouterProvider } from 'react-router-dom'
-import {Home,About,User, Extra} from './components'
+import {Home,About,User, Extra, Login, SignUp} from './components'
 import Layout from './Layout'
+import Registeration from './components/Registeration/Registeration'
 
+// More useful method ,but i don't know how to add new independent layout to it.
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />} >
+//       <Route path='' element={<Home/>} />
+//       <Route path='about' element={<About/>}/>
+//       <Route path='user/:userId' element={<User  />} />
+//       <Route path='/extra' element={<Extra />} />
+//       <Route path='/login' element={<Login />}/>
+//       <Route path='/signUp' element={<SignUp />}/>
+//     </Route>
+//   )
+// )
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />} >
-      <Route path='' element={<Home/>} />
-      <Route path='about' element={<About/>}/>
-      <Route path='user/:userId' element={<User />} />
-      <Route path='/extra' element={<Extra />} />
-    </Route>
-  )
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '',
+          element: <Home />
+        },
+        {
+          path: 'about',
+          element: <About />
+        },
+        {
+          path: 'extra',
+          element: <Extra />
+        },
+        {
+          path:'login',
+          element: <Login/>
+          
+        },
+        {
+          path: 'signUp',
+          element: <SignUp />
+        },
+        {
+          path: 'user/:userId',
+          element: <User />
+        }
+      ]
+    },{
+      path: '/registeration',
+      element: <Registeration/>
+    }
+  ]
 )
+
 function App() {
   return (
     <>
