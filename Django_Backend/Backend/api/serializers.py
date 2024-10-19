b2a3b8a91e6c5d6530cb46a387248fae83c22092
AnmolChauhan <acanmolchauhan1234@gmail.com>
2024-10-19 12:00:26 +0530

    added signup functionality in Backend

--- Django_Backend/Backend/api/serializers.py
+++ Django_Backend/Backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Party, Contact  # Import the models
+from django.contrib.auth.models import User
 
 # Serializer for Party model
 class PartySerializer(serializers.ModelSerializer):
@@ -12,4 +13,17 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = '__all__'  # Or list specific fields ['id', 'name', 'email', 'message']
+
+class SignupSerializer(serializers.ModelsSerializer):
+    class Meta:
+        model = User
+        fields = ['username,email,password']
+
+        def create(self, validated_data):
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password']
+            )
+            return user
         
--- Django_Backend/Backend/api/views.py
+++ Django_Backend/Backend/api/views.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import viewsets
 from .models import Party, Contact
-from .serializers import PartySerializer, ContactSerializer
+from .serializers import PartySerializer, ContactSerializer, SignupSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
+from rest_framework.views import APIView
 
 
 # ViewSet for Party
@@ -31,4 +32,15 @@ def contact_us(request):
             serializer.save()
             return Response({'status': 'success', 'message': 'Message sent successfully!'}, status=status.HTTP_201_CREATED)
         return Response({'status': 'error', 'message': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignupView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = SignupSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
