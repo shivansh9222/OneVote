e2ebd0075eb80499cb9b1dc4f4ea7b015f464e43
Anmol Rai <purevoidexists@gmail.com>
2024-10-21 23:01:58 +0530

    updated functions inside of login page

--- OneVote/src/components/Registeration/Login/Login.jsx
+++ OneVote/src/components/Registeration/Login/Login.jsx
@@ -3,9 +3,31 @@ import {Link} from 'react-router-dom'
 import PasswordInput from "../PasswordInput/PasswordInput";
 
 function Login(){
+    const [uniqueId , setUniqueId] = useState('');
+    const [password,setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('http://localhost:8000/api/login/' , {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({uniqueId , password})
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setUniqueId('')
+            setPassword('')
+        })
+        .catch(error => alert(error))
+    }
     return(
         <>
-            <form action="" 
+            <form action=""
+                onSubmit={handleSubmit}
                 className="flex flex-col box-border p-3 gap-y-2 text-base md:text-xl text-orange-500 rounded-lg w-max h-max items-center justify-center mx-auto bg-gray-50 shadow-lg shadow-gray-400 my-2 md:shadow-none md:w-full md:bg-white md:my-0 "
             >
                 <h1 className="text-2xl md:hidden">Login</h1>
@@ -13,9 +35,13 @@ function Login(){
                 <div className="flex flex-col mb-2">
                     <label 
                         htmlFor="userid" className="md:ubuntu-medium-italic ubuntu-light-italic mb-1 "
-                    >UserId: </label>
+                    >UniqueId: </label>
                     <input 
-                        type="text" 
+                        type="text"
+                        value={uniqueId}
+                        onChange={ (e) =>  {
+                            setUniqueId(e.target.value);
+                        }}
                         className="w-full p-1 text-start text-white rounded-lg md:rounded-full outline-none border-none bg-gray-100 focus-within:bg-orange-400 ubuntu-light-italic md:p-2 md:text-center"
                     />
                 </div>
@@ -27,11 +53,15 @@ function Login(){
                     >Password: </label>
 
                     {/* Password Input component added here */}
-                    <PasswordInput />
+                    <PasswordInput 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                     
                 </div>
 
                 <button 
+                    type="submit"
                     className="mt-2 bg-orange-400 text-white p-1 px-2 md:px-4 rounded-lg md:rounded-[24px] cursor-pointer hover:bg-orange-600"
                 >
                     Login
