647d276b5b2f27dc7b47eda0c7014f4fd6073311
Anmol Rai <purevoidexists@gmail.com>
2024-10-27 15:44:46 +0530

    assests added

---
+++ OneVote/src/assests/background.js
@@ -0,0 +1,3 @@
+const voteSuccessfulurl = 'https://i.pinimg.com/originals/55/0a/ee/550aee88cd20addc7f1e9a1ba24c7699.gif';
+
+const signUpurl = 'https://i.pinimg.com/originals/6a/22/66/6a226661a1e53c4962bd61bec779cea5.gif'
