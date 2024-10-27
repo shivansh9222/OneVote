afe580ceafce5d01f2bc30fe38a4a523ba0e79f6
Anmol Rai <purevoidexists@gmail.com>
2024-10-27 14:12:40 +0530

    added localStorage setting and retrival

--- OneVote/src/context/UserContextProvider.jsx
+++ OneVote/src/context/UserContextProvider.jsx
@@ -1,9 +1,20 @@
 import UserContext from "./UserContext";
-import { useContext , useState } from "react";
+import { useContext , useEffect, useState } from "react";
 
 const UserContextProvider = ({children}) => {
-    const [user , setUser] = useState({})
-    const [isLoggedIn , setIsLoggedIn] = useState(false)
+    const [user , setUser] = useState( ()=> {
+        const storedUser = localStorage.getItem('user');
+        return storedUser ? JSON.parse(storedUser) : {}
+    })
+    const [isLoggedIn , setIsLoggedIn] = useState( () => {
+        const storedLoggedIn = localStorage.getItem('isLoggedIn')
+        return storedLoggedIn === 'true'
+    })
+
+    useEffect( ()=> {
+        localStorage.setItem('user' , JSON.stringify(user))
+        localStorage.setItem('isLoggedIn' , isLoggedIn)
+    } , [user , isLoggedIn])
 
     return(
         <UserContext.Provider value={{user , setUser , isLoggedIn , setIsLoggedIn}}>
