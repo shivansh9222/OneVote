f925152a75282b1037f15639e58bdbc08b8bf6eb
Anmol Rai <purevoidexists@gmail.com>
2024-11-08 20:14:22 +0530

    shadow config

--- OneVote/tailwind.config.js
+++ OneVote/tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'glow-orange': '0 0 15px rgba(255, 165, 0, 0.6), 0 0 30px rgba(255, 165, 0, 0.4), 0 0 45px rgba(255, 165, 0, 0.2)',
+      },
+      animation: {
+        'pulse-glow': 'pulse-glow 1s infinite alternate',
+      },
+      keyframes: {
+        'pulse-glow': {
+          '0%': {
+                    boxShadow: '0 0 15px rgba(255, 165, 0, 0.6), 0 0 30px rgba(255, 165, 0, 0.4), 0 0 45px rgba(255, 165, 0, 0.2)' 
+                },
+          '50% ': {
+            boxShadow: '0 0 20px rgba(255, 165, 0, 0.8), 0 0 40px rgba(255, 165, 0, 0.6), 0 0 60px rgba(255, 165, 0, 0.4)'
+          } , 
+          '100%': {
+                    boxShadow: '0 0 15px rgba(255, 165, 0, 0.6), 0 0 30px rgba(255, 165, 0, 0.4), 0 0 45px rgba(255, 165, 0, 0.2)'
+                  },
+          },
+      },
+    },
   },
   plugins: [],
 }
