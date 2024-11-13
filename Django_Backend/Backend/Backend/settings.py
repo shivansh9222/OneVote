3a5fbda203abb4d2cdb733f2da3e1d568eacd536
AnmolChauhan <acanmolchauhan1234@gmail.com>
2024-11-13 18:13:36 +0530

    updates in settings.py file

--- Django_Backend/Backend/Backend/settings.py
+++ Django_Backend/Backend/Backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-d-_knlry@9d14+69ehprv912jr3-t!ii+e#o@(8155&_p847^)'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
