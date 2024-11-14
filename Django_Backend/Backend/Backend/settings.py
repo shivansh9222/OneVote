4da95a186689689a710ac9515f508f0d8b04b9b1
AnmolChauhan <acanmolchauhan1234@gmail.com>
2024-11-14 18:10:00 +0530

    updated settings.py file

--- Django_Backend/Backend/Backend/settings.py
+++ Django_Backend/Backend/Backend/settings.py
@@ -154,7 +154,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -163,7 +166,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Local development (React dev server)
-    "https://one-vote-khaki.vercel.app",  # Production URL (Vercel app)
+    "https://one-vote-khaki.vercel.app/",  # Production URL (Vercel app)
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
@@ -187,7 +190,7 @@ REST_FRAMEWORK = {
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
-    'https://one-vote-khaki.vercel.app',
+    'https://one-vote-khaki.vercel.app/',
 ]
 
 
@@ -215,5 +218,4 @@ MEDIA_URL = f'https://res.cloudinary.com/did5gvbtr/'
 
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
